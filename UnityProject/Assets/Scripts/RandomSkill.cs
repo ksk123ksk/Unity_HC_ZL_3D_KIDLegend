e3edae10b4376927324efb70dbfca6882ff4e268
ksk123ksk <49360266+ksk123ksk@users.noreply.github.com>
2020-01-02 21:32:37 +0800

    20200102上課

--- UnityProject/Assets/Scripts/RandomSkill.cs
+++ UnityProject/Assets/Scripts/RandomSkill.cs
@@ -16,6 +16,8 @@ public class RandomSkill : MonoBehaviour
     private Text textSkill;
     private string[] nameSkills = { "連續射擊", "正向箭", "背向箭", "側向箭", "血量提升", "攻擊提升", "攻速提升", "爆擊提升" };
     private GameObject panelSkill;
+    private GameObject bg;
+    private int index;
     [Header("音效")]
     public AudioClip soundScroll;
     public AudioClip soundGetSkill;
@@ -29,32 +31,35 @@ public class RandomSkill : MonoBehaviour
         aud= GetComponent<AudioSource>();
         textSkill = transform.GetChild(0).GetComponent<Text>();
         panelSkill = GameObject.Find("隨機技能");
+        bg= GameObject.Find("選擇背景");
         button.onClick.AddListener(ChooseSkill);
         StartCoroutine(ImageChoose());
     }
     private void ChooseSkill()
     {
-        panelSkill.SetActive(false);
+        panelSkill.SetActive(false);                                //隱藏隨機技能物件  
+        bg.SetActive(false);                                        //關閉半透明背景
+        print("玩家選取的技能為" + nameSkills[index]);              //紀錄玩家選取的技能
     }
 
     public IEnumerator ImageChoose()
     {
-        button.interactable = false;
+        button.interactable = false;                                //按鈕取消點選
         for (int j = 0; j < Random.Range(1, 5); j++)
         {
-            for (int i = 0; i < RandomImage.Length; i++)
+            for (int i = 0; i < RandomImage.Length; i++)            //技能滾動
             {
                 Images.sprite = RandomImage[i];
                 aud.PlayOneShot(soundScroll, 0.2f);
 
-                yield return new WaitForSeconds(speed);
+                yield return new WaitForSeconds(speed);             //延遲時間
             }
         }
-        int r = Random.Range(0, SkillImage.Length);
-        Images.sprite = SkillImage[r];
+        index = Random.Range(0, SkillImage.Length);                 //隨機
+        Images.sprite = SkillImage[index];                          //技能圖片
         aud.PlayOneShot(soundGetSkill, 0.8f);
-        textSkill.text = nameSkills[r];
-        button.interactable = true;
+        textSkill.text = nameSkills[index];                         //技能名稱
+        button.interactable = true;                                 //按鈕恢復點選
     }
 
 
