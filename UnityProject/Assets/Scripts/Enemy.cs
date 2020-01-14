abf7588c9a648d8aba4594180ff4850ed9beaa1e
ksk123ksk <49360266+ksk123ksk@users.noreply.github.com>
2020-01-14 21:31:24 +0800

    20200114上課

---
+++ UnityProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Enemy : MonoBehaviour
+{
+    [Header("怪物資料")]
+    public EnemyData data;
+    
+
+    private Animator ani;
+    private NavMeshAgent nav;
+    private Transform Player;
+
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+        nav.speed = data.speed;
+        Player = GameObject.Find("鼠王").transform;
+        nav.stoppingDistance = data.stopDistance;
+
+    }
+    private void Update()
+    {
+        Move();
+    }
+    /*摺疊 Ctrl+M+O
+      展開 Ctrl+M+L*/
+    /// <summary>
+    /// 等待
+    /// </summary>
+    private void Wait()
+    {
+
+    }
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        Vector3 posPlayer = Player.position;
+        posPlayer.y = transform.position.y;
+        transform.LookAt(posPlayer);            //看向玩家
+        ani.SetBool("跑步開關", true);
+        nav.SetDestination(Player.position);
+    }
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+        
+    }
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    /// <param name="damage">接收玩家給予的傷害值</param>
+    private void Hit(float damage)
+    {
+
+    }
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+}
---
+++ UnityProject/Assets/Scripts/EnemyData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "怪物資料", menuName="夜影/怪物資料")]
+public class EnemyData : ScriptableObject
+{
+    [Header("移動速度"), Range(0, 10)]
+    public float speed;
+    [Header("血量"), Range(100, 5000)]
+    public float hp;
+    [Header("攻擊力"), Range(10, 1000)]
+    public float atk;
+    [Header("冷卻時間"), Range(1, 10)]
+    public float cd;
+    [Header("停止距離"), Range(1, 100)]
+    public float stopDistance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
