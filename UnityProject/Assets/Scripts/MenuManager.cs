a68c8b53d7050ec578dd4f775d3afe044f6e1ce4
ksk123ksk <49360266+ksk123ksk@users.noreply.github.com>
2020-02-25 19:36:54 +0800

    20200225發布

---
+++ UnityProject/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("玩家資料")]
+    public PlayerData dataPlayer;
+
+    public void BuyHp_500()
+    {
+        dataPlayer.Maxhp += 500;
+        dataPlayer.hp = dataPlayer.Maxhp;
+    }
+    public void BuyAtk_50()
+    {
+        dataPlayer.atk += 50;
+        
+    }
+    public void LoadLevel()
+    {
+        dataPlayer.hp = dataPlayer.Maxhp;
+        SceneManager.LoadScene(1);
+    }
+
+}
